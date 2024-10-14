80ce58c8a0cf8a0eaea90be71b4aabc65717cf91
DavidMSanders <dwspokemon@gmail.com>
2024-10-13 19:32:17 -0500

    Did up to the user selection of initial piece +some functions +checks to make sure userinput is valid

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ char makeBoard(char even, char odd, int outIt, int inIt);
 void printBoard();
 void turn();
 void getUserInput();
+int convertLetter(char a);
+bool pieceCanMove(int row, int col, char playerT);
 
 //Main function
 int main() {
@@ -75,7 +77,7 @@ void printBoard() //for printing the board after every turn (we can add the grap
     printf("\n");
     for(int i=0; i<8; i++)
     {
-        printf("%d  ",8-i);
+        printf("%d  ", 8-i);
         for(int j=0; j<8;j++)
         {
             printf("%c  ", board[i][j]);
@@ -91,8 +93,41 @@ void turn()
     {
         printf("It's Black's turn!\n");
         printf("Please enter the piece you want to move.\n");
-        char input[3];
+        char input[3]; //used for initial location
+        //char input2[3]; //will be used for the end location later
         getUserInput(input);
+        int i= 7-(input[1]-'1');
+        int j = convertLetter(input[0]);
+    //checks and makes user reinput
+    while(board[i][j] != 'b' || !(pieceCanMove(i, j, playerTurn)))
+    {
+        if(board[i][j] != 'b' )
+        {
+            printf("This is not your piece!\n");
+            printf("The value at this spot is: %c\n", board[i][j]);
+            printf("Please enter the location of the piece you'd like to move.\n");
+            getUserInput(input);
+            i= 7-(input[1]-'1');
+            j = convertLetter(input[0]);
+        }
+        if(!(pieceCanMove(i, j, playerTurn)))
+        {
+            printf("This piece you chose cannot move.\n");
+            printf("The value at this spot is: %c\n", board[i][j]);
+            printf("Please enter a different location of the piece you'd like to move.\n");
+            getUserInput(input);
+            i= 7-(input[1]-'1');
+            j = convertLetter(input[0]);
+        }
+    }
+    if(board[i][j] == 'b')
+    {
+        printf("This is your piece!\n");
+        printf("The value at this spot is: %c\n", board[i][j]);
+        pieceCanMove(i, j, playerTurn);
+    }
+    
+
         
     }
     else
@@ -113,5 +148,55 @@ void getUserInput(char input[])
         printf("\nYou inputed: %s\n",input);
     }
     }
+int convertLetter(char a) //converts the letter from location to its corresponding int value
+{
+   int j;
+        if(a == 'a' ||a == 'A')
+        {
+            j=0;
+        }
+        else if(a == 'b' || a =='B')
+        {
+            j=1;
+        }
+        else if(a == 'c' ||a =='C')
+        {
+            j=2;
+        }
+        else if(a == 'd' ||a =='D')
+        {
+            j=3;
+        }
+        else if(a == 'e' ||a =='E')
+        {
+            j=4;
+        }
+        else if(a == 'f' ||a =='F')
+        {
+            j=5;
+        }
+        else if(a == 'g' ||a =='G')
+        {
+            j=6;
+        }
+        else if(a == 'h' ||a =='H')
+        {
+            j=7;
+        } 
+        return j;
+}
+bool pieceCanMove(int i, int j, char playerT)
+{
+    if(playerT == 'b')
+    {
+    if((board[i+1][j-1] == '1') || (board[i+1][j+1] == '1') || ((board[i+1][j-1] == 'r') && (board[i+2][j-2] == '1')) || (board[i+1][j+1] == 'r') && (board[i+2][j+2] == '1'))
+    {
+        printf("Has open spot to move!\n");
+        return true;
+    }
+    }
+    printf("Does not have open spot.\n");
+    return false;
+}
 
 
